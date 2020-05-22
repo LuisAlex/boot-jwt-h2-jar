c1b0cafc2880fee192064b0c2d143ebfa165df13
alex <alex@javado.com>
2020-05-21 23:38:09 -0500

    first commit

---
+++ src/main/java/com/javado/stl/bootjwth2jar/BootJwtH2JarApplication.java
@@ -0,0 +1,13 @@
+package com.javado.stl.bootjwth2jar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJwtH2JarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootJwtH2JarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javado/stl/bootjwth2jar/BootJwtH2JarApplicationTests.java
@@ -0,0 +1,16 @@
+package com.javado.stl.bootjwth2jar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootJwtH2JarApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
